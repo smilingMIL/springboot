5f03b5c9df060dd536b5c4410359e8a90defcd4c
smile <1529592482@qq.com>
2022-03-01 11:20:09 +0800

    添加docker_boot项目的注释。

--- docker_boot/src/main/java/com/docker/DockerBootMain.java
+++ docker_boot/src/main/java/com/docker/DockerBootMain.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+/**
+ * 这个是springboot 部署到 docker上。
+ */
 @SpringBootApplication
 @MapperScan("com.docker.mapper") //import tk.mybatis.spring.annotation.MapperScan;
 public class DockerBootMain {
